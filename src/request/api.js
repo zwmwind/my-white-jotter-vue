06c23a2b5cf983533f9c41a7dade49915347be30
MyOMEN <zhangweiming1995@qq.com>
2021-12-25 16:13:25 +0800

    [add] 1. 封装axios

---
+++ src/request/api.js
@@ -0,0 +1,2 @@
+// 刚刚封装的axios
+import request from './axios'
---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    用户名: <input type="text" v-model="this.loginForm.username" placeholder="Please input username">
+    密码: <input type="password" v-model="this.loginForm.password" placeholder="Please input password">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      responseResult: []
+    }
+  },
+  methods: {
+    login () {
+      this.$axios
+        .post('/login', {
+          username: this.loginForm.username,
+          password: this.loginForm.password
+        })
+    }
+  }
+}
+</script>
